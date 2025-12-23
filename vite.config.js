44facb97a2be5c9050f597a478747875b0136b42
rohan7310 <yadavrohan7310@gmail.com>
2025-12-23 14:48:17 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MEDCURE/", // 👈 repo name EXACT (case-sensitive)
+  base: "/MEDCURE/", // ✅ trailing slash REQUIRED
 });
