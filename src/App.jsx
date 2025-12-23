df4dd10e28f2a57738f9c89da9458be51f06be48
rohan7310 <yadavrohan7310@gmail.com>
2025-12-23 15:16:46 -0500

    Switch to HashRouter for application routing
    Refactor App component to use HashRouter for routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Home";
-import Appointment from "./components/Appointment";
-import Login from "./components/Login";
-import Doctor from "./components/Doctor";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Navbar />
-          <Home />
-        </>
-      ),
-    },
-    {
-      path: "/appointment",
-      element: (
-        <>
-          <Navbar />
-          <Appointment />
-        </>
-      ),
-    },
-    {
-      path: "/doctors",
-      element: (
-        <>
-          <Navbar />
-          <Doctor />
-        </>
-      ),
-    },
-    {
-      path: "/login",
-      element: (
-        <>
-          <Navbar />
-          <Login />
-        </>
-      ),
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
